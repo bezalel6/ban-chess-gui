2e6a698e6966b2b82d11b0d7a90f0150e32604b0
bezalel6 <bezalel3250@gmail.com>
2025-09-05 05:28:10 +0300

    chore: Clean up temporary test files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,4 @@ import { render } from 'preact'
 import './styles.css'
 import { App } from './app.tsx'
 
-// Import Stockfish test for browser console
-import './stockfish-test'
-
 render(<App />, document.getElementById('app')!)
